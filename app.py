0fc6c060dfadf2f056c04d6c12ce674c7f44e3be
Lkasta <lkastabackup@gmail.com>
2025-07-09 19:56:59 -0300

    feat(routes): created pix payment generator

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 from dotenv import load_dotenv
+from datetime import datetime, timedelta
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from repository.database import db
 
-from models import payment
+from models.payment import Payment
+from payments.pix import Pix
 
 load_dotenv()
 app = Flask(__name__)
@@ -12,13 +14,32 @@ app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 db.init_app(app)
 
-@app.route('/payments/pix', methods=['POST'])
+@app.route('/payments/pix/', methods=['POST'])
 def create_payment_pix():
-  return jsonify({"message", "The payment has been created!"})
+  data = request.get_json()
+
+  if 'value' not in data:
+    return jsonify({"message": "Value is not null!"}), 400
+  
+  expiration_date = datetime.now() + timedelta(minutes=30)
+
+  new_payment = Payment(value=data['value'], expiration_date=expiration_date)
+  pix_obj = Pix()
+  data_payment_pix = pix_obj.create_payment()
+
+  new_payment.bank_payment_id = data_payment_pix["bank_payment_id"]
+  new_payment.qr_code = data_payment_pix["qr_code_path"]
+
+  db.session.add(new_payment)
+  db.session.commit()
+  return jsonify({
+    "message": "The payment has been created!",
+    "payment": new_payment.to_dict()   
+  })
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def pix_confirmation():
-  return jsonify({"message", "The payment has been confirmed!"})
+  return jsonify({"message": "The payment has been confirmed!"})
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page():
